dc463dbcdee8b1114a76b2eb2d2c7c7c229fc2ef
ao <mc4@outlook.jp>
2026-02-03 03:15:51 +0900

    create 435/a.cpp

---
+++ abc/435/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int f(int n) {
+	if (n <= 1) return 1;
+	else return n + f(n - 1);
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+	cout << f(n) << endl;
+	return 0;
+}
