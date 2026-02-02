fddc479afd03c931e1632d72fd06eb84f125adc8
ao <mc4@outlook.jp>
2026-02-03 01:44:21 +0900

    create 438/a.cpp

---
+++ abc/438/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main(void) {
+	int d, f;
+	cin >> d >> f;
+
+	while (f <= d) {
+		f += 7;
+	}
+	cout << f - d << endl;
+
+	return 0;
+}
