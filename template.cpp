7be127e2c08f6c58105e04d9082d029e69fd9f47
ao81 <mc4@outlook.jp>
2026-02-05 22:38:15 +0900

    create template.cpp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using pl = pair<int, int>;
+using vl = vector<int>;
+using vvl = vector<vector<int>>;
+
+int main(void) {
+	
+}
