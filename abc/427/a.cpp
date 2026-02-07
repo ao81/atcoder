824cb43bf34896f584ff72bdc8611b99db0f2deb
ao81 <mc4@outlook.jp>
2026-02-07 13:32:19 +0900

    create abc/427/a.cpp

---
+++ abc/427/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	string s;
+	cin >> s;
+	int del = (s.length() + 1) / 2;
+	s.erase(del - 1, 1);
+	cout << s << endl;
+	return 0;
+}
