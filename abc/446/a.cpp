2c8c247bb6331ccfe21420340d8863f29bb41077
ao81 <mc4@outlook.jp>
2026-02-21 22:28:17 +0900

    create abc/446/a.cpp

---
+++ abc/446/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	string s;
+	cin >> s;
+	s[0] = tolower(s[0]);
+	cout << "Of" << s << endl;
+	return 0;
+}
