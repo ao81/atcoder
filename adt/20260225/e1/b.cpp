aa2121e6a42248ebc031f1508211452c0adbf9eb
ao81 <mc4@outlook.jp>
2026-03-02 12:04:05 +0900

    adt/20260225/e1/b.cpp

---
+++ adt/20260225/e1/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	int d, f;
+	cin >> d >> f;
+	for (; f <= d; f += 7);
+	cout << f - d << endl;
+	return 0;
+}
