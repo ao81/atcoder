65479218711c3523ac7fdc561c3dbe80534f8682
ao81 <mc4@outlook.jp>
2026-03-02 12:00:04 +0900

    adt/20260225/e1/a.cpp

---
+++ adt/20260225/e1/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	string s, t;
+	cin >> s >> t;
+	if (s == "AtCoder" && t == "Land") {
+		cout << "Yes" << endl;
+	} else {
+		cout << "No" << endl;
+	}
+	return 0;
+}
