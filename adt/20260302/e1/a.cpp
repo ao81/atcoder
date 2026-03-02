a152da39930b7237aee69c15dc8b60b9ec8d2f4f
ao81 <mc4@outlook.jp>
2026-03-02 18:55:40 +0900

    adt/20260302/e1/a.cpp

---
+++ adt/20260302/e1/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	string s;
+	cin >> s;
+	cout << s[0] << "UPC" << endl;
+	return 0;
+}
