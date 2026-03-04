36069491d484ed5f04fdb1031c3d124dbac6427d
ao81 <mc4@outlook.jp>
2026-03-04 23:17:50 +0900

    adt/20260302/e2/b.cpp

---
+++ adt/20260302/e2/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	int s;
+	cin >> s;
+	if (s >= 200 && s <= 299) cout << "Success" << endl;
+	else cout << "Failure" << endl;
+	return 0;
+}
