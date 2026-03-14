a54fdc609767772ab06828843a3fb2cf12bfa9a9
ao81 <mc4@outlook.jp>
2026-03-14 19:52:45 +0900

    sensen10/a.cpp

---
+++ sensen10/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	int a, b;
+	cin >> a >> b;
+	cout << ((a * b % 2) ? "Odd" : "Even") << endl;
+	return 0;
+}
