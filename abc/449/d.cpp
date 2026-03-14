840db36741b4d9185541cf4cadf274b6ab7ad0ba
ao81 <mc4@outlook.jp>
2026-03-14 22:56:25 +0900

    abc/449/d.cpp

--- abc/449/d.cpp
+++ abc/449/d.cpp
@@ -7,26 +7,25 @@ using pl = pair<ll, ll>;
 using vl = vector<ll>;
 using vvl = vector<vector<ll>>;
 
-ll cntEven(ll l, ll r) {
-	if (l > r) return 0;
-	auto d = [](ll n) -> ll {
-		return (n >= 0) ? n / 2 : (n - 1) / 2;
-	};
-	return d(r) - d(l - 1);
-}
-
 int main(void) {
 	ll l, r, d, u;
 	cin >> l >> r >> d >> u;
 	ll ans = 0;
+	auto f = [](ll l, ll r) -> ll {
+		if (l > r) return 0;
+		auto d = [](ll n) -> ll {
+			return (n >= 0) ? n / 2 : (n - 1) / 2;
+		};
+		return d(r) - d(l - 1);
+	};
 	rep(x, l, r) {
 		ll ax = abs(x);
 		if (ax % 2 == 0) {
 			ll yl = max(-ax, d), yr = min(ax, u);
 			if (yl <= yr) ans += yr - yl + 1;
 		}
-		ans += cntEven(d, min(-ax - 1, u));
-		ans += cntEven(max(ax + 1, d), u);
+		ans += f(d, min(-ax - 1, u));
+		ans += f(max(ax + 1, d), u);
 	}
 	cout << ans << endl;
 	return 0;
