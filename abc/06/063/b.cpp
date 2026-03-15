51a90e5c7754d07a7ea11d143184f4e4b6ed1ca1
ao81 <mc4@outlook.jp>
2026-03-15 18:53:28 +0900

    abc/06/063/b.cpp

---
+++ abc/06/063/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	string s; cin >> s;
+	set<char> a(all(s));
+	if (a.size() == s.size()) cout << "yes" << endl;
+	else cout << "no" << endl;
+	return 0;
+}
