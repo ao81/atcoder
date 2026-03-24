4dece4a0a02dbaaf77603a1057bc26d19a24f451
ao81 <mc4@outlook.jp>
2026-03-25 00:44:05 +0900

    adt/20260323/e2/a.cpp

---
+++ adt/20260323/e2/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	int x, y, z = 1; cin >> x >> y;
+	while (y--) z *= 2;
+	cout << x * z << endl;
+	return 0;
+}
