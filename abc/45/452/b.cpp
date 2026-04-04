d7218f913698f18baae8f3a5f125aff0636bce27
ao81 <mc4@outlook.jp>
2026-04-04 21:12:17 +0900

    abc/45/452/b.cpp

---
+++ abc/45/452/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using pl = pair<ll, ll>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+
+int main(void) {
+	int h, w; cin >> h >> w;
+	rep(j, 0, h) {
+		rep(i, 0, w) {
+			if (i == 0 || i == w-1 || j == 0 || j == h-1) {
+				cout << "#";
+			} else {
+				cout << ".";
+			}
+		}
+		cout << "\n";
+	}
+	return 0;
+}
