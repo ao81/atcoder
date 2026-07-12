2f1f5f97a8becadfd0a74899b4cfb5a6c63953b8
ao81 <mc4@outlook.jp>
2026-07-12 15:04:25 +0900

    テンプレートに INF, V, chmin/chmax を追加

--- !template.cpp
+++ !template.cpp
@@ -6,6 +6,10 @@ using ll = long long;
 using pl = pair<ll, ll>;
 using vl = vector<ll>;
 using vvl = vector<vector<ll>>;
+const ll INF = 1ll << 60;
+template <class T> using V = vector<T>;
+template <class A, class B> void chmax(A& l, const B& r) { if (l < r) l = r; }
+template <class A, class B> void chmin(A& l, const B& r) { if (r < l) l = r; }
 
 int main(void) {
 
